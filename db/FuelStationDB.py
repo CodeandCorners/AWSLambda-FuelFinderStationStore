9ab0c56dabf5ddc590258f348abab91e1f6044e4
CodeAndCorners <18582901+CodeandCorners@users.noreply.github.com>
2026-08-05 23:08:05 +0100

    done

--- db/FuelStationDB.py
+++ db/FuelStationDB.py
@@ -1,1 +1,35 @@
+from models.FuelStationDBDataClasses import FuelStation
 
+
+
+def saveFuelStations(
+    stations: list[FuelStation],
+    dynamoDb
+) -> None:
+    print(f"inserting {len(stations)} Fuel stations into fuel-stations")
+    with dynamoDb.Table("fuel-stations").batch_writer() as batch:
+        for station in stations:
+            batch.put_item(
+                Item={
+                    "id": station.id,
+                    "name": station.name,
+                    "createdAt": station.createdAt,
+                    "ttl": station.ttl,
+                    "geohash": station.geohash,
+                    "location": {
+                        "addressLine1": station.location.address_line_1,
+                        "postcode": station.location.postcode,
+                        "latitude": station.location.latitude,
+                        "longitude": station.location.longitude
+                    },
+                    "openingTimes": {
+                        "monday": vars(station.openingTimes.monday),
+                        "tuesday": vars(station.openingTimes.tuesday),
+                        "wednesday": vars(station.openingTimes.wednesday),
+                        "thursday": vars(station.openingTimes.thursday),
+                        "friday": vars(station.openingTimes.friday),
+                        "saturday": vars(station.openingTimes.saturday),
+                        "sunday": vars(station.openingTimes.sunday),
+                    }
+                }
+            )
--- lambda_function.py
+++ lambda_function.py
@@ -3,13 +3,17 @@ import boto3
 from connectors.FuelFinderOAuthConnector import getFuelFinderOAuthAccessToken
 from connectors.FuelStationConnector import getFuelStations
 from db.TokenDB import getAccessToken, saveAccessToken
+from models.FuelStationHttpResponseDataClasses import FuelStationResponse
 from models.FuelAuthDataClasses import BearerTokenResponse
-from models.FuelStationDBDataClasses import createFuelStationFromResponse
+from models.FuelStationDBDataClasses import createFuelStationFromResponse, FuelStation
 import urllib3
+from db.FuelStationDB import saveFuelStations
+from datetime import datetime, timedelta
 
 #http
 secrets = boto3.client("secretsmanager")
 http = urllib3.PoolManager()
+maxBatchNumberForFuelStationsApi = 99
 
 #db
 tokenTTLInSeconds = 1800
@@ -41,10 +45,33 @@ def retrieveOrSaveBearerToken() -> BearerTokenResponse:
         return fuelFinderBearerToken
 
 
+def fuelStationGetAndInsert(bearerToken: BearerTokenResponse, batchNumber: int, createdAt: int, ttl: int) -> bool:
+    fuelStationData: list[FuelStationResponse] = getFuelStations(bearerToken, http, batchNumber)
+
+    if len(fuelStationData) > 0:
+        convertedData: list[FuelStation | None] = [
+            createFuelStationFromResponse(station, createdAt, ttl, fuelStationGeoHashPrecision)
+            for station in fuelStationData
+        ]
+
+        saveFuelStations(list(filter(None, convertedData)), dynamodb)
+    print(f"Fuel stations found {len(fuelStationData)}")
+    
+    return len(fuelStationData) > 0
+
 def fuelStationOrchestration(bearerToken: BearerTokenResponse):
-        response = getFuelStations(bearerToken, http, 1)
-        converted = createFuelStationFromResponse(response[0],1,5)
-        print(converted)
+    now = datetime.now()
+    dateTimeNow: int = int(now.timestamp())
+    ttlOfDataRetrieved: int = int((now + timedelta(days=7)).timestamp())
+ 
+    batchNumber = 1
+    while batchNumber <= maxBatchNumberForFuelStationsApi:
+        print(f"Processing batch number {batchNumber}")
+        hasDataInBatch: bool = fuelStationGetAndInsert(bearerToken, batchNumber, dateTimeNow, ttlOfDataRetrieved)
+        if not hasDataInBatch:
+            print(f"No data returned for batch number {batchNumber}, stopping processing")
+            break
+        batchNumber += 1
 
 def lambda_handler(event, context):
     fuelFinderBearerToken = retrieveOrSaveBearerToken()
